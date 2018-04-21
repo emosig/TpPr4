94dc1eb24cd0fd5959170a8e0871c88289d5d2f6
Manuel Freire <manuel.freire@gmail.com>
2018-04-21 20:39:58 +0200

    pruebas movidas de sitio. Añadidas a MainTest

--- src/test/java/es/ucm/fdi/util/MainTest.java
+++ src/test/java/es/ucm/fdi/util/MainTest.java
@@ -1,19 +1,38 @@
 package es.ucm.fdi.util;
 
-import java.io.IOException;
-
 import org.junit.Test;
 
 import es.ucm.fdi.launcher.ExampleMain;
 
+import static org.junit.Assert.fail;
+
 public class MainTest {
-	@Test
-	public void test() {
-		try {
-			ExampleMain.test("D:\\EclipseJava\\Programas\\practicas\\p4\\src\\resources\\examples\\basic");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+   private void runTests(String folderName, boolean expectException) throws Exception {
+        try {
+			ExampleMain.test("src/test/resources/examples/" + folderName);
+            if (expectException) {
+                fail("Did not expect to reach this line");
+            }
+        } catch (Exception e) {
+            if ( ! expectException) {
+                throw new Exception("When running tests on " + folderName, e);
+            }
+        }
+    }
+
+    @Test
+    public void testBasic() throws Exception {
+        runTests("basic", false);
+    }
+    @Test
+    public void testAdvanced() throws Exception {
+        runTests("advanced", false);
+        runTests("advanced/ya funciona", false);
+
+    }
+    @Test
+    public void testError() throws Exception {
+		runTests("err", true);
 	}
 }
